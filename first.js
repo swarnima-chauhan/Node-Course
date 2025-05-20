a9a1fcf6e96dd8cb05ebcd65c333bb54aec05d2f
swarnima-chauhan <swarnimc130101@gmail.com>
2025-05-21 01:27:34 +0530

    first code written successfully using require

--- first.js
+++ first.js
@@ -1,1 +1,6 @@
-console.log("hello");
+const fs=require('fs');
+
+fs.writeFile("output.txt", "Writing File",(err)=>{
+    if(err) console.log("Error Occurred");
+    else console.log('File written Successfully');
+});
