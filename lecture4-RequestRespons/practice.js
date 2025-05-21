157c908ef5ff268fb83f039a1aaa6ce9984901c2
swarnima-chauhan <swarnimc130101@gmail.com>
2025-05-22 00:15:15 +0530

    sir did like this and some more modifications

--- lecture4-RequestRespons/practice.js
+++ lecture4-RequestRespons/practice.js
@@ -1,23 +1,8 @@
 const http=require('http');
 const server= http.createServer((req,res)=>{
     console.log(req.url, req.method);
-
-    if(req.url==='/'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Myntra</title></head>');
-        res.write('<body>');
-        res.write('<ol type="A">');
-        res.write('<li><a href="/home">Home</a></li><br>');
-        res.write('<li><a href="/men">Men</a></li><br>');
-        res.write('<li><a href="/women">Women</a></li><br>');
-        res.write('<li><a href="/kids">Kids</a></li><br>');
-        res.write('<li><a href="/cart">Cart</a></li><br>');
-        res.write('</ol>');
-        res.write('</body>');
-        res.write('</html>');
-        return res.end();
-    }else if(req.url==='/home'){
+    
+    if(req.url==='/home'){
         res.setHeader('Content-Type','text/html');
         res.write('<p>Welcome to Home</p>');
         return res.end();
@@ -38,7 +23,22 @@ const server= http.createServer((req,res)=>{
         res.write('<p>Welcome to Cart</p>');
         return res.end();
     }
-    return res.end();
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Myntra</title></head>');
+    res.write('<body>');
+    res.write('<ol type="A">');
+    res.write('<li><a href="/home">Home</a></li><br>');
+    res.write('<li><a href="/men">Men</a></li><br>');
+    res.write('<li><a href="/women">Women</a></li><br>');
+    res.write('<li><a href="/kids">Kids</a></li><br>');
+    res.write('<li><a href="/cart">Cart</a></li><br>');
+    res.write('</ol>');
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
+    
 
 });
 
