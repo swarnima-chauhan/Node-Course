adfc712429288e6ae7745043cdecbceb4f190d52
swarnima-chauhan <swarnimc130101@gmail.com>
2025-05-24 16:40:07 +0530

    same path , external giving some path in app.js

--- lecture10-airbnb/app.js
+++ lecture10-airbnb/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.urlencoded());
 app.use(userRouter);
-app.use(hostRouter);
+app.use("/host", hostRouter);
 
 app.use((req, res, next) => {
   res.status(404).send(`<h1>404 Your page is not found on airbnb</h1>`);
