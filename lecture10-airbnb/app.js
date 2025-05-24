6e5b0063099d08d0298308569654102c0ffeb600
swarnima-chauhan <swarnimc130101@gmail.com>
2025-05-24 16:35:08 +0530

    routing done using local modules and page not found modifications done

--- lecture10-airbnb/app.js
+++ lecture10-airbnb/app.js
@@ -3,34 +3,16 @@ const express = require("express");
 
 //local module
 const userRouter = require("./routes/userRouter");
+const hostRouter = require("./routes/hostRouter");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log(req.url, req.method);
-  next();
-});
-
 app.use(express.urlencoded());
-
 app.use(userRouter);
+app.use(hostRouter);
 
-app.get("/host/add-home", (req, res, next) => {
-  res.send(`
-        <h1> Register your home here </h1>
-        <form action="/host/add-home" method="POST">
-        <input type="text" name="houseName" placeholder="Enter the name of your house" />
-        <input type="submit"/>
-        </form>
-    `);
-});
-
-app.post("/host/add-home", (req, res, next) => {
-  console.log(req.body);
-  res.send(`
-    <h1>Home registered successfully</h1>
-    <a href="/">Go to Home</a>
-    `);
+app.use((req, res, next) => {
+  res.status(404).send(`<h1>404 Your page is not found on airbnb</h1>`);
 });
 
 app.listen(3000, () => {
