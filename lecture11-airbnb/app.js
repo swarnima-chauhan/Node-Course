ad2dbec60aa3dabf7b54de3957b89ffb165602e8
swarnima-chauhan <swarnimc130101@gmail.com>
2025-05-25 01:01:37 +0530

    successfully added home.css using public folder and attaching throung express.static

--- lecture11-airbnb/app.js
+++ lecture11-airbnb/app.js
@@ -15,6 +15,8 @@ app.use(express.urlencoded());
 app.use(userRouter);
 app.use("/host", hostRouter);
 
+app.use(express.static(path.join(rootDir, "public")));
+
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
