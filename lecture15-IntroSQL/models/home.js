382f520e4c52b5c63dcb51a71644b6c532d6f0f6
swarnima-chauhan <swarnimc130101@gmail.com>
2025-06-23 17:07:16 +0530

    starting lecture15

---
+++ lecture15-IntroSQL/models/home.js
@@ -0,0 +1,64 @@
+//core modules
+const fs = require("fs");
+const path = require("path");
+const rootDir = require("../utils/pathUtil");
+const Favourite = require("./favourite");
+const homeDataPath = path.join(rootDir, "data", "homes.json");
+
+module.exports = class Home {
+  constructor(houseName, price, location, rating, photoUrl) {
+    this.houseName = houseName;
+    this.price = price;
+    this.location = location;
+    this.rating = rating;
+    this.photoUrl = photoUrl;
+  }
+
+  save() {
+    Home.fetchAll((registeredHomes) => {
+      if (this.id) {
+        //EDIT HOME
+        registeredHomes = registeredHomes.map((home) => {
+          if (home.id === this.id) {
+            return this;
+          }
+          return home;
+        });
+      } else {
+        //ADD HOME
+        this.id = Math.random().toString();
+        registeredHomes.push(this);
+      }
+
+      fs.writeFile(homeDataPath, JSON.stringify(registeredHomes), (error) => {
+        console.log("File Writing Concluded", error);
+      });
+    });
+  }
+
+  static fetchAll(callback) {
+    fs.readFile(homeDataPath, (err, data) => {
+      if (!err) {
+        callback(JSON.parse(data));
+      } else {
+        callback([]);
+      }
+    });
+  }
+
+  static findById(homeId, callback) {
+    this.fetchAll((homes) => {
+      const homeFound = homes.find((home) => home.id === homeId);
+      callback(homeFound);
+    });
+  }
+
+  static deleteById(homeId, callback) {
+    this.fetchAll((homes) => {
+      homes = homes.filter((home) => home.id !== homeId);
+      fs.writeFile(homeDataPath, JSON.stringify(homes), (error) => {
+        Favourite.deleteById(homeId, callback);
+      });
+    });
+  }
+};
---
+++ lecture15-IntroSQL/routes/hostRouter.js
@@ -0,0 +1,20 @@
+//External MOdules
+const express = require("express");
+const hostRouter = express.Router();
+
+//local modules
+const hostController = require("../controllers/hostController");
+
+hostRouter.get("/add-home", hostController.getAddHome);
+
+hostRouter.post("/add-home", hostController.postAddHome);
+
+hostRouter.get("/host-home-list", hostController.getHostHomes);
+
+hostRouter.get("/edit-home/:homeId", hostController.getEditHome);
+
+hostRouter.post("/edit-home", hostController.postEditHome);
+
+hostRouter.post("/delete-home/:homeId", hostController.postDeleteHome);
+
+module.exports = hostRouter;
