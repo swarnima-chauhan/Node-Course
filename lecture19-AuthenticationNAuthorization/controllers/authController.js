de8cfdef5e0fa32a8931cfb6e2c5747edf27e147
swarnima-chauhan <swarnimc130101@gmail.com>
2025-07-08 21:30:17 +0530

    Using Express Validator 19.6

--- lecture19-AuthenticationNAuthorization/controllers/authController.js
+++ lecture19-AuthenticationNAuthorization/controllers/authController.js
@@ -1,3 +1,5 @@
+const { check, validationResult } = require("express-validator");
+
 exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     pageTitle: "Login",
@@ -11,19 +13,90 @@ exports.getSignUp = (req, res, next) => {
     pageTitle: "SignUp",
     currentPage: "SignUp",
     isLoggedIn: false,
+    errors: [],
+    oldInput: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      userType: "",
+    },
   });
 };
 
-exports.postSignUp = (req, res, next) => {
-  console.log(req.body);
-  //res.cookie("isLoggedIn", true);
-  //req.session.isLoggedIn = true;
-  res.redirect("/");
-};
+exports.postSignUp = [
+  check("firstName")
+    .trim()
+    .isLength({ min: 2 })
+    .withMessage("First Name should be atleast 2 characters long")
+    .matches(/^[A-Za-z\s]+$/)
+    .withMessage("First Name should contain only aplhabets"),
+
+  check("lastName")
+    .matches(/^[A-Za-z\s]*$/)
+    .withMessage("Last Name should contain only aplhabets"),
+
+  check("email")
+    .isEmail()
+    .withMessage("Please enter a valid email")
+    .normalizeEmail(),
+
+  check("password")
+    .isLength({ min: 8 })
+    .withMessage("Password should be 8 characters long")
+    .matches(/[A-Z]/)
+    .withMessage("Password should contain one UpperCase")
+    .matches(/[a-z]/)
+    .withMessage("Password should contain one LowerCase")
+    .matches(/[0-9]/)
+    .withMessage("Password should contain one Number")
+    .matches(/[@#$&*]/)
+    .withMessage("Password should contain one special character")
+    .trim(),
+
+  check("confirmPassword")
+    .trim()
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Password do not match");
+      }
+      return true;
+    }),
+
+  check("userType")
+    .notEmpty()
+    .withMessage("Please selet a user type")
+    .isIn(["guest", "host"])
+    .withMessage("Invalid user Type"),
+
+  check("terms")
+    .notEmpty()
+    .withMessage("Please Accept the terms and conditions")
+    .custom((value, { req }) => {
+      if (value != "on") {
+        throw new Error("Please accept the trems and conditions");
+      }
+      return true;
+    }),
+
+  (req, res, next) => {
+    const { firstName, lastName, email, password, userType } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).render("auth/signup", {
+        pageTitle: "Signp",
+        currentPage: "signup",
+        isLoggedIn: false,
+        errors: errors.array().map((err) => err.msg),
+        oldInput: { firstName, lastName, email, password, userType },
+      });
+    }
+    res.redirect("/login");
+  },
+];
 
 exports.postLogin = (req, res, next) => {
   console.log(req.body);
-  //res.cookie("isLoggedIn", true);
   req.session.isLoggedIn = true;
   res.redirect("/");
 };
