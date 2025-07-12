06e03922ff264b28000bfe3ddcb72d861e98f102
swarnima-chauhan <swarnimc130101@gmail.com>
2025-07-12 21:58:27 +0530

    starting fullstack todo project

--- lecture21-JSONreq/todo-backend/app.js
+++ lecture21-JSONreq/todo-backend/app.js
@@ -3,100 +3,21 @@ const path = require("path");
 
 //External module
 const express = require("express");
-const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
-const multer = require("multer");
-
-//local module
-const storeRouter = require("./routes/storeRouter");
-const hostRouter = require("./routes/hostRouter");
-const authRouter = require("./routes/authRouter");
-const rootDir = require("./utils/pathUtil");
-const errorsController = require("./controllers/errors");
 const mongoose = require("mongoose");
+const DB_PATH =
+  "mongodb+srv://root:root@completecoding.bswvdui.mongodb.net/todo?retryWrites=true&w=majority&appName=CompleteCoding";
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
-
-const store = new MongoDBStore({
-  uri: "mongodb+srv://root:root@completecoding.bswvdui.mongodb.net/airbnb?retryWrites=true&w=majority&appName=CompleteCoding",
-  collection: "sessions",
-});
-
-app.use(
-  session({
-    secret: "KnowledgeGate AI with Complete Coding",
-    resave: false,
-    saveUninitialized: true,
-    store: store,
-  })
-);
-
-const randomString = (length) => {
-  const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let result = "";
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
-};
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, randomString(10) + "-" + file.originalname);
-  },
-});
-
-const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === "image/png" ||
-    file.mimetype === "image/jpg" ||
-    file.mimetype === "image/jpeg"
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
-
-const multerOptions = {
-  storage,
-  fileFilter,
-};
+const errorsController = require("./controllers/errors");
 
 app.use(express.urlencoded());
-app.use(multer(multerOptions).single("photo"));
 app.use(express.static(path.join(rootDir, "public")));
-app.use("/uploads", express.static(path.join(rootDir, "uploads")));
-app.use("/host/uploads", express.static(path.join(rootDir, "uploads")));
-
-app.use((req, res, next) => {
-  req.isLoggedIn = req.session.isLoggedIn;
-  next();
-});
-
-app.use(authRouter);
-app.use(storeRouter);
-app.use("/host", (req, res, next) => {
-  if (req.isLoggedIn) {
-    next();
-  } else {
-    res.redirect("/login");
-  }
-});
-app.use("/host", hostRouter);
 
 app.use(errorsController.pageNotFound);
 
 mongoose
-  .connect(
-    "mongodb+srv://root:root@completecoding.bswvdui.mongodb.net/airbnb?retryWrites=true&w=majority&appName=CompleteCoding"
-  )
+  .connect(DB_PATH)
   .then(() => {
     console.log("Connected to MongoDB");
     app.listen(3000, () => {
